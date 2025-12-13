3ec1a39804bff5f69ef63358360eba9f502e5fc9
datnt0801 <tiendata3308@gmail.com>
2025-12-13 09:54:16 +0700

    fix: deploy

--- src/database/repositories/base.repository.ts
+++ src/database/repositories/base.repository.ts
@@ -1,4 +1,21 @@
-import { ObjectLiteral, Repository } from 'typeorm';
+import {
+  EntityManager,
+  EntityTarget,
+  FindOneOptions,
+  ObjectLiteral,
+} from 'typeorm';
 
-export class BaseRepository<Entity extends ObjectLiteral> extends Repository<Entity> {  }
-   
+export class BaseRepository<T extends ObjectLiteral> {
+  constructor(
+    protected readonly entity: EntityTarget<T>,
+    protected readonly manager: EntityManager,
+  ) {}
+
+  findOne(options: FindOneOptions<T>): Promise<T | null> {
+    return this.manager.findOne(this.entity, options) as Promise<T | null>;
+  }
+
+  save(entity: T): Promise<T> {
+    return this.manager.save(this.entity, entity);
+  }
+}
--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -4,7 +4,6 @@ import { OrderStatus, OrderType } from 'src/constants/enum.constant';
 import { PaymentDto } from 'src/modules/payment/dto/payment.dto';
 import { BuffetRepository } from 'src/database/repositories/buffet.repository';
 import { SetRepository } from 'src/database/repositories/set.repository';
-import { OrderEntity } from 'src/database/entities/orders.entity';
 
 @Injectable()
 export class PaymentService {
