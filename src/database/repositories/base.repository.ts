b51272750e88bf8c31088a4954b19ca28b1210fc
datnt0801 <tiendata3308@gmail.com>
2025-12-16 21:22:34 +0700

    final

--- src/database/repositories/base.repository.ts
+++ src/database/repositories/base.repository.ts
@@ -1,21 +1,5 @@
-import {
-  EntityManager,
-  EntityTarget,
-  FindOneOptions,
-  ObjectLiteral,
-} from 'typeorm';
+import { ObjectLiteral, Repository } from 'typeorm';
 
-export class BaseRepository<T extends ObjectLiteral> {
-  constructor(
-    protected readonly entity: EntityTarget<T>,
-    protected readonly manager: EntityManager,
-  ) {}
-
-  findOne(options: FindOneOptions<T>): Promise<T | null> {
-    return this.manager.findOne(this.entity, options) as Promise<T | null>;
-  }
-
-  save(entity: T): Promise<T> {
-    return this.manager.save(this.entity, entity);
-  }
-}
+export class BaseRepository<
+  Entity extends ObjectLiteral,
+> extends Repository<Entity> {}
